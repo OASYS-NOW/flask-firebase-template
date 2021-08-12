2882347081b448ef265c59daac3ab2688384e7e0
Francis Behnen <francis.behnen@gmail.com>
2021-08-12 05:13:57 +0200

    Configure Flask, Firebase and the folder structure to work nicely together

--- server/src/app.py
+++ server/src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, make_response
+from flask import Flask, render_template, make_response, send_from_directory
 import os
 import time
 
@@ -43,6 +43,8 @@ def index():
     # 3
     response.headers['Cache-Control'] = 'public, max-age=300, s-maxage=600'
     return response
+
+
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
